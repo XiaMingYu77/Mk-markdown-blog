f2dd07c2438b8717836e901baf12c0c4c24c0f66
XiaMingYu <xiamingyu@meituan.com>
2023-09-25 14:23:38 +0800

    eslint配置

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
+// eslint-disable-next-line
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import legacy from '@vitejs/plugin-legacy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    legacy({
+      // 设置目标浏览器，browserslist 配置语法
+      targets: ['> 0.2% and not dead'],
+    }),
+  ],
 })
