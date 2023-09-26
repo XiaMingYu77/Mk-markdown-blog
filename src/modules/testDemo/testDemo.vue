69cd0888e17ea1d9870284e9676a734da0f522ef
XiaMingYu <xiamingyu@meituan.com>
2023-09-26 10:04:30 +0800

    测试pr

--- src/modules/testDemo/testDemo.vue
+++ src/modules/testDemo/testDemo.vue
@@ -5,9 +5,8 @@
 <script setup>
 import request from '@utils/request.js';
 
-function test(){
-  try{
-    request.get('/api/test');
-  }catch(e){}
+async function test(){
+  let data = await request.get('/api/test');
+  console.log(data);
 }
 </script>
