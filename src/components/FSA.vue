0c0f05a54f239e7fbb17b4e0a8a0b8ddd806286b
XiaMingYu <xiamingyu@meituan.com>
2023-10-08 14:42:55 +0800

    开屏动画基础内容

--- src/components/FSA.vue
+++ src/components/FSA.vue
@@ -1,10 +1,19 @@
 <template>
-  <div class="FSA-area" ref="fsa">hahahahsfawfgagf</div>
+  <div class="FSA-area" ref="fsa">
+    <article class="text">
+      <RotateCube></RotateCube>
+      <div class="title">
+        <h1>MK-blog</h1>
+        <span style="color:#eecabc">一款可私有化部署的博客</span>
+      </div>
+    </article>
+  </div>
 </template>
 
 <script setup>
-import { defineEmits, ref, onMounted } from 'vue';
-import createRibbons from '@components/coloredRibbon.js'
+import { ref, onMounted } from 'vue';
+import createRibbons from '@components/coloredRibbon.js';
+import RotateCube from './RotateCube.vue';
 // eslint-disable-next-line
 const emit = defineEmits(["animationEnd"]);
 const fsa = ref();
@@ -13,6 +22,7 @@ onMounted(() => {
   // 生成背景
   createRibbons({
     "body_": fsa.value,
+    "color": '#B9D1ED'
   })
 })
 
@@ -29,5 +39,15 @@ setTimeout(() => {
   right: 0;
   top: 0;
   bottom: 0;
+
+  .text{
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    margin: auto;
+    width: 800px;
+  }
 }
 </style>
